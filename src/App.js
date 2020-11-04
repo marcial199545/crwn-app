ed17597e747f2af3120e96c7efa45251071b08fb
Marcial <marcial.jimenez.caballero@gmail.com>
2020-11-04 13:16:36 -0800

    broken our home page into smaller components and finished styling MenuItem components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import HomePage from './components/homepage';
-
+import HomePage from './pages/HomePage';
+import './App.scss';
 function App() {
     return (
         <div>
